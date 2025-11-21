55cabb7aaf0d1e42cefe0fb4bc9fc0f3d7d2936d
Jhon Fredy Berrio Marin <jhoxsan@gmail.com>
2025-11-21 16:54:25 -0500

    build: update types for vue

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,22 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
+  plugins: [
+    vue(),
+    tailwindcss()
+  ],
   css: {
     postcss: {
       config: './postcss.config.js'
     },
   },
-  plugins: [
-    vue()
-  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
 });
