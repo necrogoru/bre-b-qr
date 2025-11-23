92620279ad1a9952601377758b04f7423db50269
Jhon Fredy Berrio Marin <jhoxsan@gmail.com>
2025-11-23 17:07:55 -0500

    build: add ssg

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
-import { createApp } from 'vue';
-import App from './App.vue';
+import { ViteSSG } from 'vite-ssg'
+import { h } from 'vue'
+import { RouterView } from 'vue-router'
+import App from './App.vue'
 
-import './assets/main.css';
+import './assets/main.css'
 
-createApp(App).mount('#app');
+export const createApp = ViteSSG(
+  { render: () => h(RouterView) },
+  { routes: [{ path: '/', component: App }] },
+)
